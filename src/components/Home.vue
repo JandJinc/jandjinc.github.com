041fa4e9a3198b97d1a17cd2b69ece2b1e4c8336
Merc4tor <joostkkoch@gmail.com>
2022-11-21 23:09:45 +0100

    Revert "fix"
    This reverts commit f48a07664c8dfc21335b98cd54ea6a799540a35a.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,7 @@
     <div :style="{'height': '500px', 'width': '100%', 'background-color': colors[items.indexOf(selected_item)]}">
         <v-select :items="items" v-model="selected_item" variant="solo"></v-select>
     </div>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/w0AOGeqOnFY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
     <!-- <img style="text-align: left; height: 400px" :src="require('@/assets/startphotoai.jpg')">
     
     <p style="white-space: pre-line; font-size: 150%">
